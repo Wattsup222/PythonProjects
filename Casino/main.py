d152c8ae5e442bbc33e9acae9989350757c189e1
Christopher Watt <chriswatt222@gmail.com>
2025-07-04 12:17:43 +1000

    Removed all code as a different start to the project was required. Added the starting foundations to the Casino Project

--- Casino/main.py
+++ Casino/main.py
@@ -1,24 +1,54 @@
+import time
 from random import uniform
 
 
-def generate_card_balance():
-    # floating point between a and b to two decimal places
-    balance = round(uniform(100, 10000), 2)
-    return balance
+def welcome_statement():
+    message = "Welcome to Starlight City Casino"
+    message_length = len(message)
+    symbol_row = "*" * message_length
+    print(f"{symbol_row}\n{message}\n{symbol_row}")
 
 
-def deposit(available):
-    amount_viable = False
-    while not amount_viable:
-        amount = int(input("How much do you want to deposit: "))
-        if amount <= available:
-            print(f"You have successfully deposited ${amount}")
-            amount_viable = True
-        else:
-            print(f"Not enough funds to deposit ${amount}")
-    return amount
+def setup():
+    starting_capitol = round(uniform(100, 10000), 2)
+    return starting_capitol
 
 
-if __name__ == '__main__':
-    capital_available = generate_card_balance()
-    tab = deposit(capital_available)
+def main_menu():
+    time.sleep(1)
+    print("\n1: View balance\n2: View Games\n3: Quit")
+    choice = int(input("\nSelection: "))
+    return choice
+
+
+def check_value(x):
+    match x:
+        case 1:
+            balance_menu()
+        case 2:
+            game_menu()
+        case 3:
+            app_quit()
+
+
+def balance_menu():
+    pass
+
+
+def game_menu():
+    pass
+
+
+def app_quit():
+    message = "Thanks for Visiting Starlight City Casino"
+    message_length = len(message)
+    symbol_row = "*" * message_length
+    print(f"\n{symbol_row}\n{message}\n{symbol_row}")
+    quit()
+
+
+if __name__ == "__main__":
+    capitol = setup()
+    welcome_statement()
+    option = main_menu()
+    check_value(option)
