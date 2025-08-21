1ef500c74e0d7be48976c07d651a236d792aebca
Christopher Watt <chriswatt222@gmail.com>
2025-08-21 21:30:25 +1000

    Updated the program to modify the URL based on information provided. The program now retrieves the city based on the latitude and longitude values provided. All units are in metric form

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,10 +1,43 @@
 import requests
 
-base_url = "https://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&appid=32754a42fb0647dc44d5b29a10aaa115"
+API_KEY = "32754a42fb0647dc44d5b29a10aaa115"
+BASE_WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall"
+BASE_GEO_URL = "https://api.openweathermap.org/geo/1.0/direct"
 
 
-def call_weather_data():
-    response = requests.get(base_url)
+def update_geo_url(city):
+    modified_geo_url = f"{BASE_GEO_URL}?q={city}&limit=1&appid={API_KEY}"
+    return modified_geo_url
+
+
+def call_geo_data(modified_geo_url):
+    response = requests.get(modified_geo_url)
+    print(response)
+
+    if response.status_code == 200:
+        geo_data = response.json()
+        print(geo_data)
+        return geo_data
+    else:
+        print(f"Failed to retrieve geo data {response.status_code}")
+        return None
+
+
+def location(geo_data):
+    name = geo_data[0]["name"]
+    latitude = geo_data[0]["lat"]
+    longitude = geo_data[0]["lon"]
+    print(name, latitude, longitude)
+    return latitude, longitude, name
+
+
+def update_weather_url(latitude, longitude):
+    modified_weather_url = f"{BASE_WEATHER_URL}?lat={latitude}&lon={longitude}&units=metric&appid={API_KEY}"
+    return modified_weather_url
+
+
+def call_weather_data(modified_weather_url):
+    response = requests.get(modified_weather_url)
     print(response)
 
     if response.status_code == 200:
@@ -13,15 +46,20 @@ def call_weather_data():
         return weather_data
     else:
         print(f"Failed to retrieve weather data {response.status_code}")
+        return None
 
 
 def get_weather_info(weather_data):
     if weather_data:
-        print(f"{weather_data["weather"][0]["main"]}")
+        print(f"{weather_data["current"]}")
 
 
 def main():
-    weather_data = call_weather_data()
+    modified_geo_url = update_geo_url("Sydney")
+    geo_data = call_geo_data(modified_geo_url)
+    latitude, longitude, name = location(geo_data)
+    modified_weather_url = update_weather_url(latitude, longitude)
+    weather_data = call_weather_data(modified_weather_url)
     get_weather_info(weather_data)
 
 
