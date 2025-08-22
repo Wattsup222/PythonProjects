eb126bd4872ef8e4f11ae1e492e88fc9f649dac4
Christopher Watt <chriswatt222@gmail.com>
2025-08-22 23:51:10 +1000

    changes the name of a parameter in GeoAPI and added functionality to some of the methods

--- weather_app/geo_api.py
+++ weather_app/geo_api.py
@@ -1,12 +1,23 @@
-class GeoAPI:
-    def __init__(self, base_url, key, city):
-        self.base_url = base_url
-        self.key = key
-        self.city = city
+import requests
 
+
+class GeoAPI:
+    def __init__(self, base_url, api_key, city):
+        self.base_url = base_url
+        self.api_key = api_key
+        self.city = city
+        self.endpoint_url = f"{self.base_url}?q={self.city}&limit=1&appid={self.api_key}"
+        self.geo_data = None
 
     def access_data(self):
-        pass
+        response = requests.get(self.endpoint_url)
+        print(response)  # test
+
+        if response.status_code == 200:
+            self.geo_data = response.json()
+            print(self.geo_data)  # test
+        else:
+            print(f"Failed to get data from API {response.status_code}")
 
     def get_location(self):
         pass
